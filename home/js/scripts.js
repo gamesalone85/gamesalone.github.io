eac9a35459b8364e79e3488738023549e741c8ec
gamesalone85 <257934905+gamesalone85@users.noreply.github.com>
2026-03-19 00:32:50 -0600

    Update scripts.js

--- home/js/scripts.js
+++ home/js/scripts.js
@@ -1,24 +1,27 @@
 // Base para futuras funciones
 console.log("Home cargado correctamente");
-let angle = 0;
-const carousel = document.getElementById("carousel3d");
+document.addEventListener("DOMContentLoaded", function () {
 
-document.getElementById("next").addEventListener("click", () => {
-    angle -= 90;
-    carousel.style.transform = `rotateY(${angle}deg)`;
-});
+    let angle = 0;
+    const carousel = document.getElementById("carousel3d");
 
-document.getElementById("prev").addEventListener("click", () => {
-    angle += 90;
-    carousel.style.transform = `rotateY(${angle}deg)`;
-});
-
-/* CLICK EN TARJETAS */
-document.querySelectorAll(".carousel-item").forEach(item => {
-    item.addEventListener("click", () => {
-        const link = item.getAttribute("data-link");
-        if (link && link !== "#") {
-            window.location.href = link;
-        }
+    document.getElementById("next").addEventListener("click", () => {
+        angle -= 90;
+        carousel.style.transform = `rotateY(${angle}deg)`;
     });
+
+    document.getElementById("prev").addEventListener("click", () => {
+        angle += 90;
+        carousel.style.transform = `rotateY(${angle}deg)`;
+    });
+
+    document.querySelectorAll(".carousel-item").forEach(item => {
+        item.addEventListener("click", () => {
+            const link = item.getAttribute("data-link");
+            if (link && link !== "#") {
+                window.location.href = link;
+            }
+        });
+    });
+
 });
