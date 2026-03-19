d22b7e625416b074986b62f80befeaf219c9f0fa
gamesalone85 <257934905+gamesalone85@users.noreply.github.com>
2026-03-19 00:30:42 -0600

    Update scripts.js

--- home/js/scripts.js
+++ home/js/scripts.js
@@ -1,22 +1,24 @@
 // Base para futuras funciones
 console.log("Home cargado correctamente");
 let angle = 0;
-const carousel = document.querySelector(".carousel-3d");
+const carousel = document.getElementById("carousel3d");
 
-document.getElementById("next").onclick = () => {
+document.getElementById("next").addEventListener("click", () => {
     angle -= 90;
     carousel.style.transform = `rotateY(${angle}deg)`;
-};
+});
 
-document.getElementById("prev").onclick = () => {
+document.getElementById("prev").addEventListener("click", () => {
     angle += 90;
     carousel.style.transform = `rotateY(${angle}deg)`;
-};
+});
 
-/* CLICK EN IMAGEN */
+/* CLICK EN TARJETAS */
 document.querySelectorAll(".carousel-item").forEach(item => {
     item.addEventListener("click", () => {
         const link = item.getAttribute("data-link");
-        if (link) window.location.href = link;
+        if (link && link !== "#") {
+            window.location.href = link;
+        }
     });
 });
