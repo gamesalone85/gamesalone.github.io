f2a12d4798c558e84ce2491baf2a2997fa0c8b78
gamesalone85 <257934905+gamesalone85@users.noreply.github.com>
2026-03-19 00:18:04 -0600

    Update scripts.js

--- home/js/scripts.js
+++ home/js/scripts.js
@@ -1,2 +1,22 @@
 // Base para futuras funciones
 console.log("Home cargado correctamente");
+let angle = 0;
+const carousel = document.querySelector(".carousel-3d");
+
+document.getElementById("next").onclick = () => {
+    angle -= 90;
+    carousel.style.transform = `rotateY(${angle}deg)`;
+};
+
+document.getElementById("prev").onclick = () => {
+    angle += 90;
+    carousel.style.transform = `rotateY(${angle}deg)`;
+};
+
+/* CLICK EN IMAGEN */
+document.querySelectorAll(".carousel-item").forEach(item => {
+    item.addEventListener("click", () => {
+        const link = item.getAttribute("data-link");
+        if (link) window.location.href = link;
+    });
+});
