e1a1ae60be291ff3c7e50bd08186b3db82b3e6e5
gamesalone85 <257934905+gamesalone85@users.noreply.github.com>
2026-05-17 00:20:29 -0600

    Update app.js

--- chocolates/js/app.js
+++ chocolates/js/app.js
@@ -1,16 +1,80 @@
-window.addEventListener("scroll", () => {
+const modal =
+    document.getElementById(
+        "cotizacionModal"
+    );
 
-    const header =
-        document.querySelector(".header");
+const openBtn =
+    document.getElementById(
+        "openModal"
+    );
 
-    if(window.scrollY > 50){
+const closeBtn =
+    document.getElementById(
+        "closeModal"
+    );
 
-        header.style.background =
-            "rgba(0,0,0,.75)";
+openBtn.addEventListener(
+    "click",
+    () => {
 
-    }else{
-
-        header.style.background =
-            "rgba(0,0,0,.2)";
+        modal.classList.add(
+            "active"
+        );
     }
-});
+);
+
+closeBtn.addEventListener(
+    "click",
+    () => {
+
+        modal.classList.remove(
+            "active"
+        );
+    }
+);
+
+/* EVITAR RECARGA */
+
+document
+.getElementById(
+    "cotizacionForm"
+)
+.addEventListener(
+    "submit",
+    (e) => {
+
+        e.preventDefault();
+
+        alert(
+            "Solicitud enviada correctamente"
+        );
+
+        modal.classList.remove(
+            "active"
+        );
+    }
+);
+
+/* HEADER DINÁMICO */
+
+window.addEventListener(
+    "scroll",
+    () => {
+
+        const header =
+            document.querySelector(
+                ".header"
+            );
+
+        if(window.scrollY > 50){
+
+            header.style.background =
+                "rgba(0,0,0,.75)";
+
+        }else{
+
+            header.style.background =
+                "rgba(0,0,0,.2)";
+        }
+    }
+);
