00f77cdcf4a27b8f1b2172ba5d99d0b5518f0bfd
gamesalone85 <257934905+gamesalone85@users.noreply.github.com>
2026-06-06 02:02:48 -0600

    Update app.js

--- chocolates/js/app.js
+++ chocolates/js/app.js
@@ -255,3 +255,66 @@ document.addEventListener("DOMContentLoaded", () => {
         );
     }
 });
+/* ======================
+   POPUP PROMOCIONES
+====================== */
+
+const promoPopup = document.getElementById('promoPopup');
+const closePromo = document.getElementById('closePromo');
+
+closePromo.addEventListener('click', () => {
+    promoPopup.style.display = 'none';
+});
+
+/* ======================
+   CARRUSEL
+====================== */
+
+const track = document.querySelector('.carousel-track');
+const slides = document.querySelectorAll('.carousel-track a');
+
+const nextBtn = document.querySelector('.next');
+const prevBtn = document.querySelector('.prev');
+
+let index = 0;
+
+function moveCarousel(){
+    track.style.transform =
+    `translateX(-${index * 100}%)`;
+}
+
+nextBtn.addEventListener('click', () => {
+
+    index++;
+
+    if(index >= slides.length){
+        index = 0;
+    }
+
+    moveCarousel();
+
+});
+
+prevBtn.addEventListener('click', () => {
+
+    index--;
+
+    if(index < 0){
+        index = slides.length - 1;
+    }
+
+    moveCarousel();
+
+});
+
+setInterval(() => {
+
+    index++;
+
+    if(index >= slides.length){
+        index = 0;
+    }
+
+    moveCarousel();
+
+}, 5000);
